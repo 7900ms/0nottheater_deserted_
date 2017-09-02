ccc7efed9bbba14e5e6abfe958bbc23e656ad966
7900ms <7900ms@users.noreply.github.com>
2017-09-02 09:05:41 +0000

    Create sample.java

---
+++ Install_Manual/java/sample.java
@@ -0,0 +1,5 @@
+
+
+// http://www.thegeekstuff.com/2017/06/java-8-jre-jdk-install/
+// http://www.thegeekstuff.com/2010/02/java-hello-world-example-how-to-write-and-execute-java-program-on-unix-os/
+
