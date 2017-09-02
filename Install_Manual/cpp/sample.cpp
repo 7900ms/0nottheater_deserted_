80f8b363dfa8526c2089a61762dc844591c3ab92
7900ms <7900ms@users.noreply.github.com>
2017-09-02 08:55:30 +0000

    Create sample.cpp

---
+++ Install_Manual/cpp/sample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void test1();
+
+int main(){
+    test1();
+}
+
+void test1(){
+    string s = "hi";
+    cout << s << endl << endl;
+}
