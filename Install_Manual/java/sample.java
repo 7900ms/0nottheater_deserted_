b72c42aeb80258d91a6fbfefff558c4a972270ef
7900ms <7900ms@users.noreply.github.com>
2017-09-02 09:13:09 +0000

    Update sample.java

--- Install_Manual/java/sample.java
+++ Install_Manual/java/sample.java
@@ -1,4 +1,21 @@
 
+// javac Sample.java
+// java Sample
+
+class Sample {
+    public static void main(String[] args) {
+
+        test1();
+
+    }
+
+    static void test1(){
+        System.out.println("hello");
+    }
+
+
+}
+
 
 // http://www.thegeekstuff.com/2017/06/java-8-jre-jdk-install/
 // http://www.thegeekstuff.com/2010/02/java-hello-world-example-how-to-write-and-execute-java-program-on-unix-os/
